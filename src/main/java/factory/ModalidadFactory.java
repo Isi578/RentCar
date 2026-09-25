845c872e81f78fb215e3a2e748a8ce934cfe5402
Isi578 <isabela.quinterom@uqvirtual.edu.co>
2026-09-24 20:50:59 -0500

    Add ModalidadFactory with crear method
    Introduce factory class ModalidadFactory with a crear(ModalidadAlquiler) method that currently returns a placeholder string "Modalidad". Adds basic factory scaffolding for creating modality representations.

---
+++ src/main/java/factory/ModalidadFactory.java
@@ -0,0 +1,10 @@
+package factory;
+
+import model.ModalidadAlquiler;
+
+public class ModalidadFactory {
+
+    public String crear(ModalidadAlquiler modalidadAlquiler) {
+        return "Modalidad";
+    }
+}
